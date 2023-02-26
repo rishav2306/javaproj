ea50281f1bef1d9866723321ce765000c36b0ba4
rishav2306 <rishavjohnj@gmail.com>
2023-02-26 12:00:16 +0530

    new program on loops

---
+++ java_prog/src/java_prog/loops.java
@@ -0,0 +1,19 @@
+package java_prog;
+
+public class loops {
+
+	public static void main(String[] args) {
+		
+		
+	int i=0;
+	
+	do {
+		
+		System.out.println("*");
+		i++;
+		
+	}while(i>1);
+
+	}
+
+}
