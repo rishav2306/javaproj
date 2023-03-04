48ca4546451cc28a4e35d1022ed5f13f8765579a
rishav2306 <rishavjohnj@gmail.com>
2023-03-04 14:51:06 +0530

    pattern programming r under box

---
+++ java_prog/src/java_prog/pattern3.java
@@ -0,0 +1,48 @@
+package java_prog;
+
+import java.util.Scanner;
+
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+       
+		System.out.println("enter a value:");
+		
+		int n = sc.nextInt();
+		 
+		
+		for(int i=0; i<n; i++)
+		{
+			for(int j =0; j<n; j++)
+				
+			{
+				if(i==0 || i ==(n-1) || j==0 || j==(n-1))
+				{
+					System.out.print("*");
+				
+				}
+				else if(i==1 && j>2 && j<(n-3) || i>1 && i<(n-1) && j==2 || i==(n-1)/2 && j>2 && j<(n-4) || i>1 && i< (n-1) && j == (n-3) && i != (n-1)/2 )
+				{
+					
+					System.out.print("0");
+					
+				}
+				
+				else 
+				{
+					System.out.print(" ");
+				}
+	
+			}
+			System.out.println();
+			
+			
+			
+		}
+		sc.close();
+	}
+
+}
