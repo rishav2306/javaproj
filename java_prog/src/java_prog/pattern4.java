2f259ab1336a0e8264c196503118f02ebbcd3df1
rishav2306 <rishavjohnj@gmail.com>
2023-03-04 16:39:54 +0530

    mofre complex program

--- java_prog/src/java_prog/pattern4.java
+++ java_prog/src/java_prog/pattern4.java
@@ -9,7 +9,7 @@ public class pattern4 {
 
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter teh valeue:");
+		System.out.println("Enter the value:");
 		
 		int n = sc.nextInt();
 		
@@ -17,16 +17,32 @@ public class pattern4 {
 		{
 			for(int j=0; j<n; j++)
 			{
-				if(i==j || i+j==(n-1) || i==0 || j==0 || i==(n-1) || j==(n-1) || i+j ==(n-1)/2 || i-j == (n-1)/2 || j-i==(n-1)/2 || i+j == ((n-1)/2 + (n-1)))
+				if(i==j || i+j==(n-1) || i==(n-1) || j==(n-1) || i+j ==(n-1)/2 || i-j == (n-1)/2 || j-i==(n-1)/2 || i+j == ((n-1)/2 + (n-1)))
 				{
-					System.out.print("*");
+					System.out.print("   ");
 				}
 				
 				else
 				{
-					System.out.print(" ");
+					System.out.print(" * ");
 				}
 			}
+			
+			System.out.print(" ");
+			
+			for(int j=0; j<n; j++)
+			{
+				if(i==(n-1) || j==(n-1) || i+j <=(n-1)/2 || i-j >= (n-1)/2 || j-i>=(n-1)/2 || i+j >= ((n-1)/2 + (n-1)))
+				{ 
+					System.out.print("   ");
+				}
+				
+				else
+				{
+					System.out.print(" * ");
+				}
+			}
+			
 			System.out.println();
 		}
 		
