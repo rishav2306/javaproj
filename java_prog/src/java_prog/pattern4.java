49fa47bfc67f5eeb36312ce0033b8f5ad4b10d54
rishav2306 <rishavjohnj@gmail.com>
2023-03-04 15:55:27 +0530

    complex patter

---
+++ java_prog/src/java_prog/pattern4.java
@@ -0,0 +1,36 @@
+package java_prog;
+
+import java.util.Scanner;
+
+public class pattern4 {
+
+	public static void main(String[] args)
+	{
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter teh valeue:");
+		
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(i==j || i+j==(n-1) || i==0 || j==0 || i==(n-1) || j==(n-1) || i+j ==(n-1)/2 || i-j == (n-1)/2 || j-i==(n-1)/2 || i+j == ((n-1)/2 + (n-1)))
+				{
+					System.out.print("*");
+				}
+				
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
