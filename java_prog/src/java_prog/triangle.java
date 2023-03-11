e19c698d6c13b629b6d9376e8fc92c16acad2ae4
rishav2306 <rishavjohnj@gmail.com>
2023-03-11 12:34:58 +0530

    assignmentsum

---
+++ java_prog/src/java_prog/triangle.java
@@ -0,0 +1,55 @@
+package java_prog;
+
+import java.util.Scanner;
+
+public class triangle {
+
+	
+
+		public static void main(String[] args) {
+	     
+		 Scanner sc = new Scanner(System.in);
+		  
+		 System.out.println("enter an odd number :");
+		 
+		 int n = sc.nextInt();
+		 
+			/*
+			 * for(int i=0; i<n; i++) { if(i == 0 || i == n-1) { for(int j=0; j<n; j++) {
+			 * System.out.print("* ");
+			 * 
+			 * }
+			 * 
+			 * System.out.println(); }
+			 * 
+			 * else { for(int j=0; j<n; j++) { if( j == 0 || j == n-1) {
+			 * System.out.print("* "); }
+			 * 
+			 * else { System.out.print("  "); } }
+			 * 
+			 * System.out.println(); } }
+			 */
+		 
+		 for(int i = 0; i<=n/2; i++)
+		 {
+			 for(int j=0; n%2==0?j<=n:j<n; j++ )
+			 {
+				 if(j<=(n/2)+i && j>= (n/2)-i || i==n/2 && j==n)
+				 {
+					 System.out.print("* ");
+				 }
+				 
+				 else
+				 {
+					 System.out.print("  ");			
+				 }
+			 }
+			 System.out.println();
+		 }
+		 
+		 sc.close();
+
+		}
+
+	}
+
